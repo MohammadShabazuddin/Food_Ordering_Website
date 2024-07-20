4d6e41311e99ec2d5d1bf8a93dfe68562dbf5ea6
MohammadShabazuddin <44221567+MohammadShabazuddin@users.noreply.github.com>
2024-07-20 00:01:35 -0400

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,8 +38,8 @@ const placeOrder = async (req, res) => {
         })
         
           const session = await stripe.checkout.sessions.create({
-            success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `https://food-ordering-website-frontend-ta2c.onrender.com/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `https://food-ordering-website-frontend-ta2c.onrender.com/verify?success=false&orderId=${newOrder._id}`,
             line_items: line_items,
             mode: 'payment',
           });
